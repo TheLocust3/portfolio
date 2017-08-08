da8a02a156c56a209004ac45a6068e3df7e02965
TheLocust3 <jake.kinsella@gmail.com>
2017-08-08 18:48:21 -0400

    Article list

--- app/javascript/components/components/articles/Index.tsx
+++ app/javascript/components/components/articles/Index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Article } from '../../../api/articles'
 import { ConnectedState, ConnectedDispatch } from '../../containers/articles/IndexContainer';
 
 export default class Index extends React.Component<ConnectedState & ConnectedDispatch, any> {
@@ -8,11 +9,29 @@ export default class Index extends React.Component<ConnectedState & ConnectedDis
         this.props.getAllArticles();
     }
 
-    render(): JSX.Element {
-        console.log(this.props)
+    renderArticleList() {
+        if (!this.props.isReady) return;
+
         return (
             <div>
-                Articles Index
+                {this.props.articles.map((article: Article[], i: number) => {
+                    return (
+                        <div key={i}>
+                            <h2>{article.title}</h2>
+                            <div>
+                                {article.text}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    render(): JSX.Element {
+        return (
+            <div>
+                {this.renderArticleList()}
             </div>
         );
     }
