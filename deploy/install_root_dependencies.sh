ac3499b2197fa72dc2a7f160c993d7746c914e68
TheLocust3 <jake.kinsella@gmail.com>
2019-01-07 15:22:06 -0500

    Use ssl on site

--- deploy/install_root_dependencies.sh
+++ deploy/install_root_dependencies.sh
@@ -13,5 +13,12 @@ sudo ln -sf /usr/local/n/versions/node/8.6.0/bin/node /usr/bin/nodejs
 
 sudo npm install --global yarn
 
-# setup nginx + passenger
+# setup nginx
 sudo apt-get install -y nginx
+
+# install certbot
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y universe
+sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y python-certbot-nginx
