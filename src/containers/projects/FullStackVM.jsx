e83eca449b2b98f2d4be4019fbb31b3d5ba46086
TheLocust3 <jake.kinsella@gmail.com>
2019-02-10 14:36:01 -0500

    Add writeup for full stack v

--- src/containers/projects/FullStackVM.jsx
+++ src/containers/projects/FullStackVM.jsx
@@ -23,8 +23,9 @@ class Intel8080 extends React.Component {
           <title>Jake Kinsella - Project: Full Stack VM</title>
           <meta
             name="description"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus convallis."
+            content="A virtual machine built from the ground up, emulating a complete CPU with a limited
+            instruction set, building out an assembler for the emulator with plans to create higher level
+            languages on top of it."
           />
         </Helmet>
 
@@ -43,9 +44,9 @@ class Intel8080 extends React.Component {
               <Text type="headline4">Overview</Text>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus
-                convallis.
+                A virtual machine built from the ground up, emulating a complete CPU with a limited
+                instruction set, building out an assembler for the emulator with plans to create
+                higher level languages on top of it.
               </p>
 
               <br />
@@ -74,35 +75,37 @@ class Intel8080 extends React.Component {
               <Text type="headline4">Creation</Text>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus
-                convallis. Donec enim urna, hendrerit non elit nec, pellentesque sollicitudin magna.
-                Vestibulum luctus, felis vel convallis pretium, neque leo posuere eros, ac venenatis
-                elit turpis id neque. In hac habitasse platea dictumst.
+                During the last two weeks of my Fundamentals of Computer Science 1 class, we started
+                learning about building our own simple functional programming languages. The core to
+                this project was building a stack language that our new language could compile to
+                and then be run as. This idea of a stack language was new to me and I immediately
+                latched onto it, with ideas from my previous experiences of building emulators
+                flooding back.
               </p>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus
-                convallis. Donec enim urna, hendrerit non elit nec, pellentesque sollicitudin magna.
-                Vestibulum luctus, felis vel convallis pretium, neque leo posuere eros, ac venenatis
-                elit turpis id neque. In hac habitasse platea dictumst.
+                A stack language seemed, to me, to be closely related to Assembly and machine code,
+                but it could be used to build even higher level languages, something I’ve always
+                wanted to pursue. So I married these two ideas together. What if, I thought, I could
+                write an emulator to my own processor and slowly build up an entire stack around it?
+                I could start with machine code, and go from assembly, to a stack language, to
+                higher level languages interfacing with my monitor and hardware.
               </p>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus
-                convallis. Donec enim urna, hendrerit non elit nec, pellentesque sollicitudin magna.
-                Vestibulum luctus, felis vel convallis pretium, neque leo posuere eros, ac venenatis
-                elit turpis id neque. In hac habitasse platea dictumst.
+                With this pipedream in my mind, I started out building another emulator just like my
+                previous ones except that for this one, I wasn’t reading complex CPU documentation,
+                instead I was writing it. So after writing the bare minimum needed to describe my
+                CPU, I was off to programming, this time with Rust, a language I had always heard a
+                lot about but never given a shot.
               </p>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus
-                convallis. Donec enim urna, hendrerit non elit nec, pellentesque sollicitudin magna.
-                Vestibulum luctus, felis vel convallis pretium, neque leo posuere eros, ac venenatis
-                elit turpis id neque. In hac habitasse platea dictumst.
+                I took all the knowledge I had from my past programming projects, like building out
+                clean code and writing extensive tests, and completed a fully functional emulator.
+                This is where I hit the present day. As of right now, I am currently working on an
+                assembly language variant that compiles to my own machine code, and has its own more
+                complex subroutines that add functionality to a purposely limited instruction set.
               </p>
 
               <br />
@@ -110,11 +113,12 @@ class Intel8080 extends React.Component {
               <Text type="headline4">Takeaways</Text>
 
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta diam ut feugiat
-                consectetur. Mauris nec metus erat. Nulla ultricies ipsum nec augue maximus
-                convallis. Donec enim urna, hendrerit non elit nec, pellentesque sollicitudin magna.
-                Vestibulum luctus, felis vel convallis pretium, neque leo posuere eros, ac venenatis
-                elit turpis id neque. In hac habitasse platea dictumst.
+                So far it’s been a ride from the idea of just building another emulator to deciding
+                that this would be the first step of building an entire custom stack. The more I
+                continue, the more I learn about architecting code with extensible and clean design,
+                keeping in mind that I don’t know entirely where this project will go. I am only
+                starting this project despite the thousands of lines of code it already contains,
+                but I’m sure that by the end of it, I’ll be back here writing another a conclusion.
               </p>
 
               <a href="https://github.com/TheLocust3/full-stack-vm">GitHub</a>
